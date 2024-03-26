12216f6a38ffee388d2c1561fada0e80c2d41281
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2024-03-26 09:53:26 -0300

    first commit

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,17 @@
+export function Profile() {
+  return (
+    <div className="flex items-center gap-3">
+      <img
+        src="https://github.com/gabrielpramalho.png"
+        alt="Foto de Perfil Gabriel"
+        className="w-10 h-10 rounded-full"
+      />
+
+      <div className="flex flex-col">
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+  )
+}
