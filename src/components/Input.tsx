c15a9648486aa97c53aa3b62594e73aaa4fb099e
Gabriel Ramalho <biel-pedro@hotmail.com>
2024-04-23 11:35:17 -0300

    feat: create animations

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,7 +11,7 @@ interface InputControlProps extends ComponentProps<'input'> {}
 export function InputControl(props: InputControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
       {...props}
     />
   )
@@ -23,7 +23,7 @@ export function InputRoot(props: InputRootProps) {
   return (
     <div
       {...props}
-      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
     />
   )
 }
